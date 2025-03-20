26430c2dc514504115e8ac34c10f4de5243272fc
Roterodamus <89250803+roterodamus@users.noreply.github.com>
2025-03-20 13:03:19 +0100

    Update update-m8c.sh

--- update-m8c.sh
+++ update-m8c.sh
@@ -3,7 +3,7 @@
 REPO_URL="https://github.com/laamaa/m8c.git"
 DIR_NAME="m8c"
 
-sudo pacman -Syu --noconfirm --needed libserialport sdl3 gcc pkgconf make git
+sudo pacman -Sy --noconfirm --needed libserialport sdl3 gcc pkgconf make git
 
 if [ -d "$DIR_NAME" ]; then
     rm -rf "$DIR_NAME"
