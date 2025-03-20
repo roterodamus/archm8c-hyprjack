dbbe44312b1dd631ad63306a3258b79798302305
Roterodamus <89250803+roterodamus@users.noreply.github.com>
2025-03-20 12:46:57 +0100

    Add files via upload

---
+++ update-m8c.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+REPO_URL="https://github.com/laamaa/m8c.git"
+DIR_NAME="m8c"
+
+sudo pacman -Syu --noconfirm --needed libserialport sdl3 gcc pkgconf make git
+
+if [ -d "$DIR_NAME" ]; then
+    rm -rf "$DIR_NAME"
+fi
+
+git clone "$REPO_URL"
+cd "$DIR_NAME"
+
+make
+sudo make install
+
+exit
