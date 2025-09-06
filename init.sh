bd5352f0beadb89f5b832db218514130da4f261c
Roterodamus <89250803+roterodamus@users.noreply.github.com>
2025-09-06 04:06:37 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -7,17 +7,17 @@ USERNAME="$USER"
 sudo usermod -aG audio,uucp "$USERNAME"
 
 # Install the required packages
-sudo pacman -Syu --noconfirm --needed libserialport sdl3 gcc pkgconf make git hyprland kitty dolphin wofi nano brightnessctl swaybg alsa-utils a2jmidid jack2 jack-example-tools linux-headers xf86-input-libinput libinput xpad bluez bluez-utils blueberry
+sudo pacman -Syu --noconfirm --needed sway brightnessctl nano alsa-utils a2jmidid jack2 jack-example-tools linux-headers xf86-input-libinput libinput xpad bluez bluez-utils \
+hyprland kitty dolphin wofi swaybg blueberry
 
 # Make laucher script executable
 chmod +x jack-m8c.sh
 chmod +x update-m8c.sh
 
 # Download and install m8c
-git clone https://github.com/laamaa/m8c.git
-cd m8c
-make
-sudo make install
+git clone https://aur.archlinux.org/m8c.git
+cd m8c/
+makepkg -si
 cd ..
 
 # download and install bluetooth autoconnect
